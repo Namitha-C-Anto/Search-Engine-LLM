38772c08edad1bf32e611c6008b78d5f07b793cd
Namitha-C-Anto <namithaanto@gmail.com>
2025-11-11 16:18:17 +0530

    Refactor app.py to improve agent creation logic

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from langchain_groq import ChatGroq
 from langchain_community.utilities import WikipediaAPIWrapper, ArxivAPIWrapper
 from langchain_community.tools import WikipediaQueryRun, ArxivQueryRun, DuckDuckGoSearchRun 
 #from langchain.agents import initialize_agent, AgentType
-from langchain.agents import create_tool_calling_agent (and refactor logic)
+from langchain.agents import create_tool_calling_agent
 
 #from langchain.callbacks import StreamlitCallbackHandler ## allows you to communicate with all this tools
 from langchain_community.callbacks import StreamlitCallbackHandle
@@ -90,5 +90,6 @@ if prompt:=st.chat_input(placeholder="What is Machine Learning?"):
         st.session_state.messages.append({"role":"assistant", "content": response})
         #st.write(response)
         st.write(str(response))
+
 
 
