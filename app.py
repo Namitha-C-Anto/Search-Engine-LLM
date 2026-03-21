2a11adef215adaed50827350c182c3fd92c6e2b2
Namitha-C-Anto <namithaanto@gmail.com>
2026-03-22 01:40:12 +0530

    Refactor app.py for improved chat functionality

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
 wiki_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=200)
 wiki = WikipediaQueryRun(api_wrapper=wiki_wrapper)
 
-duck_wrapper = DuckDuckGoSearchAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+duck_wrapper = DuckDuckGoSearchAPIWrapper()
 search = DuckDuckGoSearchRun(api_wrapper=duck_wrapper)
 
 tools = [search, arxiv, wiki]
